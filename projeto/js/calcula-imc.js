fb799816ff0bed6bf215f5437956854a620dc0ef
Luiz Fernando Cardozo <luiz@mbpr2.local>
2017-01-30 21:28:34 -0200

    DOM pelo id

--- projeto/js/calcula-imc.js
+++ projeto/js/calcula-imc.js
@@ -1,8 +1,14 @@
-var peso = 0;
-var altura = 0;
+//cria o objeto/array paciente e pega os valores de peso e altura a partir do ID do elemento
+var paciente = {
+	"peso" : document.getElementById("peso-2").textContent,
+	"altura" : document.getElementById("altura-2").textContent
+}
 
-if(altura > 0){
-	var imc = peso / (altura * altura); 
+
+if(paciente.altura > 0){
+	var imc = paciente.peso / (paciente.altura * paciente.altura); 
+	var tdImc = document.getElementById("imc-2");
+	tdImc.textContent = imc;
 	console.log(imc);
 } else {
 	console.log("Altura inválida");
