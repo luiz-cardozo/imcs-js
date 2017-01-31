842e6269999e2722e78ddabc21f52f2028c47df0
Luiz Fernando Cardozo <luiz@mbpr2.local>
2017-01-30 22:26:10 -0200

    criada função que calcula o imc em array de ppacientes

--- projeto/js/calcula-imc.js
+++ projeto/js/calcula-imc.js
@@ -1,15 +1,37 @@
 //cria o objeto/array paciente e pega os valores de peso e altura a partir do ID do elemento
-var paciente = {
-	"peso" : document.getElementById("peso-2").textContent,
-	"altura" : document.getElementById("altura-2").textContent
-}
+// var paciente = {
+// 	"peso" : document.getElementById("peso-2").textContent,
+// 	"altura" : document.getElementById("altura-2").textContent
+// }
+
+//pega os dados com base na classe utilizada
+var dadosPacientes = document.getElementsByClassName("paciente");
 
 
-if(paciente.altura > 0){
-	var imc = paciente.peso / (paciente.altura * paciente.altura); 
-	var tdImc = document.getElementById("imc-2");
-	tdImc.textContent = imc;
-	console.log(imc);
-} else {
-	console.log("Altura inválida");
+
+for(var i = 0; i <= dadosPacientes.length -1; i++){
+	var pacienteAtual = dadosPacientes[i];
+
+	//declarado como variável para fazer atribuição posterior
+	var imcPaciente = pacienteAtual.getElementsByClassName("info-imc")[0]
+
+	var paciente = {
+		nome : pacienteAtual.getElementsByClassName("info-nome")[0].textContent,
+		peso : pacienteAtual.getElementsByClassName("info-peso")[0].textContent,
+		altura : pacienteAtual.getElementsByClassName("info-altura")[0].textContent,
+	}
+
+	if(paciente.altura != 0){
+		var calculoImc = paciente.peso / (paciente.altura * paciente.altura); 
+		imcPaciente.textContent = calculoImc;
+		console.log(calculoImc);
+	}else{
+		console.log("Altura inválida");
+	}
+
 }
+
+// 
+// 	var tdImc = document.getElementById("imc-2");
+// 	tdImc.textContent = imc;
+// 	console.log(imc);
